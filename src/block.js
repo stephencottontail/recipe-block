74ea7455c6dcf5bc1a21c8bf1d561865f5f64cb2
Stephen Dickinson <stephencottontail@me.com>
2020-04-12 23:31:47 -0600

    Set up how the block appears in the editor

--- src/block.js
+++ src/block.js
@@ -7,16 +7,37 @@ registerBlockType( 'sc/recipe', {
 	icon: 'format-aside',
 	category: 'common',
 	attributes: {
+		title: {
+			source: 'html',
+			selector: '.wp-block-sc-recipe__title',
+			default: ''
+		},
 		blathering: {
 			source: 'html',
 			selector: '.wp-block-sc-recipe__blathering',
+			default: ''
+		},
+		ingredients: {
+			source: 'html',
+			selector: '.wp-block-sc-recipe__ingredients',
+			default: ''
+		},
+		instructions: {
+			source: 'html',
+			selector: '.wp-block-sc-recipe__instructions',
 			default: ''
 		}
 	},
 	edit,
 	save: () => {
-		return el( 'div', {
-			className: 'sc-recipe-front'
-		}, 'Hello from the front!' )
+		return (
+			el(
+				'div',
+				{
+					className: 'sc-recipe-front'
+				},
+				'Hello from the front!'
+			)
+		)
 	}
 } )
--- src/edit.js
+++ src/edit.js
@@ -9,7 +9,6 @@ class Edit extends Component {
 	render() {
 		const { attributes, setAttributes, className } = this.props
 
-		console.log( attributes )
 		return (
 			el(
 				'div',
@@ -19,15 +18,62 @@ class Edit extends Component {
 				el(
 					RichText,
 					{
-						className: `${className}__blathering`,
-						value: attributes.blathering,
-						placeholder: 'Write your pretentious irrelevant blathering about this recipe...',
+						tagName: 'h2',
+						className: `${className}__title`,
+						value: attributes.title,
+						placeholder: 'Make sure to use a minimum of 4 adjectives when naming your recipe...',
 						keepPlaceholderOnFocus: true,
-						multiline: true,
 						onChange: ( content ) => {
-							setAttributes( { blathering: content } )
+							setAttributes( { title: content } )
 						}
 					}
+				),
+				el(
+					'div',
+					{
+						className: `${className}__content`,
+					},
+					el(
+						RichText,
+						{
+							className: `${className}__blathering`,
+							value: attributes.blathering,
+							placeholder: 'Write your pretentious irrelevant blathering about this recipe...',
+							keepPlaceholderOnFocus: true,
+							multiline: true,
+							onChange: ( content ) => {
+								setAttributes( { blathering: content } )
+							}
+						}
+					),
+					el(
+						RichText,
+						{
+							tagName: 'ul',
+							className: `${className}__ingredients`,
+							value: attributes.ingredients,
+							placeholder: 'Ingredients...',
+							keepPlaceholderOnFocus: true,
+							multiline: 'li',
+							onChange: ( content ) => {
+								setAttributes( { ingredients: content } )
+							}
+						}
+					),
+					el(
+						RichText,
+						{
+							tagName: 'ol',
+							multiline: 'li',
+							className: `${className}__instructions`,
+							value: attributes.instructions,
+							placeholder: 'Instructions...',
+							keepPlaceholderOnFocus: true,
+							onChange: ( content ) => {
+								setAttributes( { instructions: content } )
+							}
+						}
+					)
 				)
 			)
 		)
